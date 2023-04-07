a2ab9dc7bada751163204e5b3921d01dbceca99a
sujanst98644 <sujan.st98644@gmail.com>
2023-04-07 11:37:53 +0545

    hell

--- donate.php
+++ donate.php
@@ -7,14 +7,22 @@
   require 'donateerror.php';
   
 ?>
-<div class="container-fluid red-background size">
-	<div class="row">
-		<div class="col-md-6 offset-md-3">
-			<h1 class="text-center">Donate</h1>
-			<hr class="white-bar">
-		</div>
-	</div>
-</div>
+<!-- donate section -->
+<div class="container-fluid red-background">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">Donate The Blood</h1>
+						<hr class="white-bar">
+						<p class="text-center pera-text">
+						Donating blood is important because it can save lives. Blood is needed for a wide range of medical procedures, including surgeries, 
+						cancer treatments, and treating medical conditions such as anemia and blood disorders. When you donate blood, you are helping to ensure 
+						that there is a sufficient supply of blood for those who need it.
+						Donating blood is an important act of kindness and generosity that can make a significant difference in the lives of others.
+					</p>
+						<a href="donate.php" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
+					</div>
+				</div>
+			</div>
 <div class="container size">
 	<div class="row">
 		<div class="col-md-6 offset-md-3 form-container">
--- include/header.php
+++ include/header.php
@@ -13,7 +13,38 @@
 		<link rel="stylesheet" href="css/custom.css">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
 	</head>
-<?php 
-            include 'config.php';
-            include 'navigation.php';
+    <?php 
+    include 'config.php';
+    if (!isset($_SESSION['role_id'])) {
+    include 'navigation.php';
+    }    
+    session_start();
+    
+    // Check if the user is logged in
+    if (isset($_SESSION['role_id'])) {
+        // User is logged in, so get their role_id from the database
+        $role_id = $_SESSION['role_id'];
+        $query = "SELECT role_id FROM users WHERE role_id = '$role_id'";
+        $result = mysqli_query($conn, $query);
+        
+        if (mysqli_num_rows($result) == 1) {
+            // User exists, so get their role_id and show/hide navigation items based on role_id
+            $row = mysqli_fetch_assoc($result);
+            $role_id = $row['role_id'];
+            
+            if ($role_id == 1) {
+                include 'donornav.php';
+                // Admin user, so show all navigation items
+                // code to show all navigation items here
+            } elseif ($role_id == 2) {
+                include 'staffnav.php';
+    // Regular user, so show some navigation items
+                // code to show some navigation items here
+            } elseif ($role_id == 3) {
+                include 'receptionistsnav.php';
+    // Another type of user, so show some different navigation items
+                // code to show some different navigation items here
+            }
+        }
+    }
 ?>
